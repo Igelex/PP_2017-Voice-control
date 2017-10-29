348e3ed2f430db6389b2d1cc38638c2218b21414
igelex <sergatam@yandex.com>
2017-10-29 13:32:31 +0100

    init server

---
+++ Server/app.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+});
+module.exports = app;
